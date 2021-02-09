7dfbf160065ea7867d9c6ad66ece61d80a8fa5c8
wensimin <hukaizhiyu2@gmail.com>
2021-02-09 13:31:14 +0800

    dev 提交

--- src/main/java/com/wensimin/dynamic/ip/utils/ResourceUtils.java
+++ src/main/java/com/wensimin/dynamic/ip/utils/ResourceUtils.java
@@ -3,7 +3,7 @@ package com.wensimin.dynamic.ip.utils;
 import java.util.ResourceBundle;
 
 public class ResourceUtils {
-	private static final ResourceBundle bundle = ResourceBundle.getBundle("config");
+	private static final ResourceBundle bundle = ResourceBundle.getBundle("config-dev");
 
 	public static String getString(String key) {
 		return bundle.getString(key);
